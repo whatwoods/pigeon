8ca09b7a01219245f70251a1bcdc98a6412cf82c
Way <way@WaydeMacBook-Air.local>
2026-06-04 22:05:44 +0800

    Refine noise overlay styling

--- src/App.tsx
+++ src/App.tsx
@@ -1466,6 +1466,7 @@ function NoiseOverlay() {
     <svg className="noise-overlay" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
       <filter id="noiseFilter">
         <feTurbulence type="fractalNoise" baseFrequency="0.65" numOctaves="3" stitchTiles="stitch" />
+        <feColorMatrix type="saturate" values="0" />
       </filter>
       <rect width="100%" height="100%" filter="url(#noiseFilter)" />
     </svg>
